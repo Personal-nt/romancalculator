239b4f108bc38f27de6d3e0342669c703eef760f
0xNordian <85548493+0xNordian@users.noreply.github.com>
2023-11-10 04:12:57 +0100

    calculations wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,162 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { Calc } from './utils/calc'
+// import ButtonWrapper from "./components/ButtonWrapper";
 
-function App() {
-  const [count, setCount] = useState(0)
+function Calculator() {
+  const [total, setTotal] = useState(0);
+  const [current, setCurrent] = useState<any[]>([]);
+  const [operation, setOperation] = useState<any[]>([])
+  const [operator, setOperator] = useState("");
+  // const [clear, setClear] = useState(false);
+
+
+  const handleOperation = (input: string | number, operator?: string) => {
+    if (typeof input === "number") {
+      if (operator === "") {
+        // setTotal((prevTotal) => prevTotal + input);
+        setCurrent((prev) => [...prev, input]);
+      } else {
+        setCurrent((prev) => [...prev, input]);
+      }
+    }
+    if (typeof input === "string") {
+      setOperator(input);
+      const currentNum = current.join("");
+      calculateResult(parseInt(currentNum), operator ? operator : input);
+    }
+  };
+
+  const clearTotal = () => {
+    setTotal(0);
+    setCurrent([]);
+    setOperator("");
+    setOperation([]);
+  };
+
+  const calculateResult = (currentNum: number, operator: string) => {
+    switch (operator) {
+      case "+":
+        setTotal((prevTotal) => prevTotal + currentNum);
+        break;
+      case "-":
+        setTotal((prevTotal) => prevTotal - currentNum);
+        break;
+      case "*":
+        setTotal((prevTotal) => prevTotal * currentNum);
+        break;
+      case "/":
+        setTotal((prevTotal) => prevTotal / currentNum);
+        break;
+      default:
+        setTotal(currentNum); // If no operator is provided, set the current number as the total.
+    }
+    setCurrent([]);
+    setOperator("");
+  };
+
+  function numberToRoman(num: number): string {
+    if (isNaN(num) || num < 0 || num >= 4000) {
+      throw new Error("Number out of range for Roman numerals (1-3999)");
+    }
+  
+    const romanNumerals: [string, number][] = [
+      ["M", 1000],
+      ["CM", 900],
+      ["D", 500],
+      ["CD", 400],
+      ["C", 100],
+      ["XC", 90],
+      ["L", 50],
+      ["XL", 40],
+      ["X", 10],
+      ["IX", 9],
+      ["V", 5],
+      ["IV", 4],
+      ["I", 1],
+    ];
+  
+    let result = "";
+    let remaining = num;
+  
+    for (const [roman, value] of romanNumerals) {
+      while (remaining >= value) {
+        result += roman;
+        remaining -= value;
+      }
+    }
+  
+    return result;
+  }
+  const test = total;
+
+  const romanNumber = numberToRoman(test);
+  console.log("total:", total);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 dark:bg-gray-900">
+      <div className=" w-80 bg-white rounded-lg shadow-md p-12 dark:bg-gray-800 text-teal-400">
+        <div className="border-b border-gray-200 pb-4 dark:border-gray-700">
+          <div className="text-right text-3xl font-bold mb-2 text-gray-500 dark:text-gray-300">{romanNumber}</div>
+          <div className="text-right text-xl text-gray-400 dark:text-gray-600">{total}</div>
+        </div>
+        <div className="grid grid-cols-4 gap-4 mt-6">
+          <button onClick={() => clearTotal()} className="col-span-2 rounded-lg bg-pink-900 text-white">
+            Clear
+          </button>
+          <button onClick={() => handleOperation("/", operator)} className="rounded-lg bg-teal-900">
+            /
+          </button>
+          <button onClick={() => handleOperation("*", operator)} className="rounded-lg bg-teal-900">
+            *
+          </button>
+          <button onClick={() =>  handleOperation(7, operator)} className="rounded-lg bg-gray-600 bg-opacity-30 hover:bg-opacity-50 scale-[1] hover:scale-[103%] transition-transform duration-75">
+            VII
+          </button>
+          <button onClick={() =>  handleOperation(8, operator)} className="rounded-lg bg-gray-600 bg-opacity-30 hover:bg-opacity-50 scale-[1] hover:scale-[103%] transition-transform duration-75">
+            VIII
+          </button>
+          <button onClick={() =>  handleOperation(9, operator)} className="rounded-lg bg-gray-600 bg-opacity-30 hover:bg-opacity-50 scale-[1] hover:scale-[103%] transition-transform duration-75">
+            IX
+          </button>
+          <button onClick={() => handleOperation("-", operator)} className="rounded-lg bg-teal-900">
+            -
+          </button>
+          <button onClick={() =>  handleOperation(4, operator)} className="rounded-lg bg-gray-600 bg-opacity-30 hover:bg-opacity-50 scale-[1] hover:scale-[103%] transition-transform duration-75">
+            IV
+          </button>
+          <button onClick={() =>  handleOperation(5, operator)} className="rounded-lg bg-gray-600 bg-opacity-30 hover:bg-opacity-50 scale-[1] hover:scale-[103%] transition-transform duration-75">
+            V
+          </button>
+          <button onClick={() =>  handleOperation(6, operator)} className="rounded-lg bg-gray-600 bg-opacity-30 hover:bg-opacity-50 scale-[1] hover:scale-[103%] transition-transform duration-75">
+            VI
+          </button>
+          <button onClick={() => handleOperation("+", operator)} className="rounded-lg bg-teal-900">
+            +
+          </button>
+          <button onClick={() =>  handleOperation(1, operator)} className="rounded-lg bg-gray-600 bg-opacity-30 hover:bg-opacity-50 scale-[1] hover:scale-[103%] transition-transform duration-75">
+            I
+          </button>
+          <button onClick={() =>  handleOperation(2, operator)} className="rounded-lg bg-gray-600 bg-opacity-30 hover:bg-opacity-50 scale-[1] hover:scale-[103%] transition-transform duration-75">
+            II
+          </button>
+          <button onClick={() =>  handleOperation(3, operator)} className="rounded-lg bg-gray-600 bg-opacity-30 hover:bg-opacity-50 scale-[1] hover:scale-[103%] transition-transform duration-75">
+            III
+          </button>
+          <button className="rounded-lg bg-teal-800 row-span-2">
+            =
+          </button>
+          <button className="rounded-lg bg-gray-600 bg-opacity-30 hover:bg-opacity-50 scale-[1] hover:scale-[103%] transition-transform duration-75 col-span-2">
+            0
+          </button>
+          <button disabled className="rounded-lg">
+          
+          </button>
+
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <small className="mt-6 w-[300px] text-teal-400 text-opacity-40 text-center">Currently this calculator doesnt work following the PEMDAS model</small>
+    </div>
+  );
 }
 
-export default App
+export default Calculator;
---
+++ src/utils/calc.ts
@@ -0,0 +1,25 @@
+export class Calc {
+    // public a: number = 0;
+    // public b: number = 0;
+    
+    // constructor({a, b}: NumTypes) {
+    //     this.setA(a);
+    //     this.setB(b);
+    // }
+
+    // setA(a: number): void {
+    //     if (typeof a === 'number') {
+    //         this.a = a;
+    //     }
+    // }
+
+    // setB(b: number): void {
+    //     if (typeof b === 'number') {
+    //         this.b = b;
+    //     }
+    // }
+
+    add(a: number, b: number){
+        return a + b
+    }
+}
